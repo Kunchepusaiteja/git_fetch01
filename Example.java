25f884ebf5464683e115d439abbfa9edf83b0962
Kunchepusaiteja <kunchepusaiteja710@gmail.com>
2024-09-13 11:21:31 +0530

    after update

--- Example.java
+++ Example.java
@@ -1,6 +1,7 @@
 class Example{
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello world");
         int a = 10;
         int b = 20;
         System.out.println(a+" "+b);
